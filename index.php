54fb581c225061b646e029c44bf9892e9243c371
troysirat <troysirat@gmail.com>
2015-05-19 07:50:36 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,82 @@
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>SWCTA SkillsUSA</title>
+  <meta name="description" content="Southwest Carrier & Tech Academy's SkillsUSA chapter website.">
+  <meta name="author" content="Troy Sirat">
+
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+  <script src="js/scripts.js"></script>
+  <style>
+    html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	
+	
+	vertical-align: baseline;
+}
+
+    
+  </style>
+  <script type="text/javascript">
+$(function() {
+  $('form').submit(function(){
+    $.post('http://example.com/upload', function() {
+      window.location = 'http://google.com';
+    });
+    return false;
+  });
+});
+</script>
+</head>
+
+<body style="height: 100%; font-family: helvetica;">
+  <div style="background-color: #0071bc; border-bottom: 10px solid #ffd72e; margin-bottom: 20px; padding-bottom: 20px;">
+    <img></img>
+    <h1 style="text-align: center; color: white; font-size: 80px;">SWCTA SkillsUSA</h1>
+  </div>
+    <div style="width: 70%; margin: 0 auto; clear: both; ">
+        <h2 >What is SkillsUSA?</h2>
+        <p style="margin-top: 20px;">SkillsUSA is a <strong>Career and Student Oraganization(CTSO)</strong> that focuses on <strong>industry and leadership abilities.</strong> Members of SkillsUSA are given oportunities to <strong>utilize these skills and apply them to the real world.</strong></p>
+        <div style="margin: 0 auto; width: 70%; line-height: 200%; margin-top: 50px;">
+            <a href="form.php" style="color: white; text-decoration: none;"><div  style="float: left; text-align: center; width: 200px; height: 150px; background-color: #ed1c24; color: white; padding-top: 50px;">
+            <h4>Need to input your information?</h4><p>Click Here</p>
+            </div></a>
+            <a href="#" style="color: white; text-decoration: none;"><div  style="float: left; text-align: center; width: 200px; height: 150px; background-color: #ed1c24; color: white; padding-top: 50px; margin-left: 45px;">
+                <h4>Want to purchase </br>chapter items?</h4><p>Click Here</p>
+            </div></a>
+            <a href="#" style="color: white; text-decoration: none;"><div  style="float: left;  text-align: center; width: 200px; height: 150px; background-color: #ed1c24; color: white; padding-top: 50px; margin-left: 45px;">
+                <h4>Have questions?</h4><p>Click Here</p>
+            </div></a>
+        </div>
+        
+        <p style="clear: both; padding-top: 50px; padding-bottom: 20px;">Click <strong><a href="http://skillsusa.org/competitions/skillsusa-championships/contest-descriptions/" target="_blank" style="color: #ed1c24; text-decoration: none;">here</a> </strong>to see a list of all available competitions at SkillsUSA.</p>
+    
+    </div>
+    <div style="border-top: 10px solid #ffd72e; clear: both; background-color: #0071bc; height: 153px;">
+        
+        <div style="margin: 10px; float: left;">
+            <a href="#"><i class="fa fa-facebook-square" style="font-size: 40px; color: white;"></i></a>
+            <a href="#"><i class="fa fa-instagram" style="font-size: 40px; color: white; margin-left: 5px;"></i></a>
+        </div>
+        <h3 style="color: white; text-align: center; margin-top: 50px;">SWCTA SkillsUSA Chapter Copyright 2015</h3>
+        
+    </div>
+</body>
+</html>
